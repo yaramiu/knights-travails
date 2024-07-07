d93b2a81a36dcc6e5e08eb707da2cbd83d97f051
yaramiu <yaramiu@gmail.com>
2024-07-06 23:37:03 -0400

    Implement knightMoves

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ class Square {
     this.row = row;
     this.column = column;
     this.validMoves = [];
+    this.path = [];
   }
 }
 
@@ -65,6 +66,42 @@ function addValidMoves() {
   });
 }
 
+function knightMoves(start, end) {
+  const startSquare = findSquare(start[0], start[1]);
+  startSquare.path.push([startSquare.row, startSquare.column]);
+  const endSquare = findSquare(end[0], end[1]);
+
+  const queue = [];
+  queue.push(startSquare);
+
+  const visitedSquares = [];
+  while (queue.length > 0) {
+    const currentSquare = queue.shift();
+
+    if (currentSquare === endSquare) {
+      break;
+    }
+
+    if (!visitedSquares.includes(currentSquare)) {
+      visitedSquares.push(currentSquare);
+      currentSquare.validMoves.forEach((square) => {
+        square.path = currentSquare.path.concat([[square.row, square.column]]);
+        queue.push(square);
+      });
+    }
+  }
+
+  const numMoves = endSquare.path.length - 1;
+  console.log(`=> You made it in ${numMoves} moves!  Here's your path:`);
+  endSquare.path.forEach((square) => {
+    console.log(square);
+  });
+}
+
 createSquares();
 addValidMoves();
-console.log(board);
+// knightMoves([0, 0], [1, 2]);
+// knightMoves([0, 0], [3, 3]);
+// knightMoves([3, 3], [0, 0]);
+// knightMoves([0, 0], [7, 7]);
+knightMoves([3, 3], [4, 3]);
